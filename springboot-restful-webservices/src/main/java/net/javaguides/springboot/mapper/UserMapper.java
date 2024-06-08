a0936024f4d32b39b69a82197cd6a5398cf59284
Eduardo Vicente <eduardoartemis@gmail.com>
2024-06-07 20:36:04 -0500

    Video 38

---
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package net.javaguides.springboot.mapper;
+
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+
+public class UserMapper {
+
+    //Convert User JPA entity to UserDto
+    public static UserDto mapToUserDto(User user) {
+        UserDto userDto = new UserDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail()
+        );
+        return userDto;
+    }
+
+    //Convert UserDto JPA entity to User
+    public static User mapToUser(UserDto userDto) {
+        User user = new User(
+                userDto.getId(),
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getEmail()
+        );
+
+        return user;
+    }
+}
