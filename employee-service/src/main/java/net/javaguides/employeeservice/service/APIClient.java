fc6584335a1ad5f4a2cc960c30e7944e9ea250cd
Eduardo Vicente <eduardoartemis@gmail.com>
2024-06-19 16:01:39 -0500

    Video 98

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
@@ -0,0 +1,14 @@
+package net.javaguides.employeeservice.service;
+
+import net.javaguides.employeeservice.dto.DepartmentDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+public interface APIClient {
+
+    @GetMapping("api/departments/{deparment-code}")
+    DepartmentDto getDepartment(@PathVariable("deparment-code") String code);
+
+}
--- employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,11 +8,12 @@ import net.javaguides.employeeservice.exception.EmailAlreadyExistsException;
 import net.javaguides.employeeservice.exception.ResourceNotFoundException;
 import net.javaguides.employeeservice.mapper.AutoEmployeeMapper;
 import net.javaguides.employeeservice.repository.EmployeeRepository;
+import net.javaguides.employeeservice.service.APIClient;
 import net.javaguides.employeeservice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -22,8 +23,16 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    /*
     @Autowired
     private RestTemplate restTemplate;
+     */
+    /*
+    @Autowired
+    private WebClient webClient;
+     */
+    @Autowired
+    private APIClient apiClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -44,10 +53,21 @@ public class EmployeeServiceImpl implements EmployeeService {
                 () -> new ResourceNotFoundException("Employee", "id", id)
         );
 
+        /*
         ResponseEntity<DepartmentDto> responseEntity  = restTemplate.getForEntity("http://localhost:8080/api/departments/"
                         + employee.getDepartamentCode(),
                 DepartmentDto.class);
         DepartmentDto departmentDto = responseEntity.getBody();
+         */
+
+        /*
+        DepartmentDto departmentDto = webClient.get().
+                uri("http://localhost:8080/api/departments/"+ employee.getDepartamentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+        */
+        DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartamentCode());
 
         APIResponseDto apiResponseDto = new APIResponseDto();
 
